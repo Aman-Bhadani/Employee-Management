8dfd2699cd219e8fa824470cc486411c2c190905
Aman Bhadani <bhadani.aman.911@gmail.com>
2022-07-24 19:38:16 +0530

    initial push

---
+++ src/data/index.js
@@ -0,0 +1,84 @@
+const employeesData = [
+    {
+      id: 1,
+      firstName: 'Rahul',
+      lastName: 'kumar',
+      email: 'rahul@example.com',
+      salary: '10000',
+      date: '2022-04-11'
+    },
+    {
+      id: 2,
+      firstName: 'Vishal',
+      lastName: 'Kumar',
+      email: 'viahal@example.com',
+      salary: '20000',
+      date: '2022-01-17'
+    },
+    {
+      id: 3,
+      firstName: 'Anuj',
+      lastName: 'Srivastava',
+      email: 'anuj@example.com',
+      salary: '30000',
+      date: '2022-02-15'
+    },
+    {
+      id: 4,
+      firstName: 'Piyush',
+      lastName: 'Bansal',
+      email: 'piyush@example.com',
+      salary: '40000',
+      date: '2022-04-09'
+    },
+    {
+      id: 5,
+      firstName: 'Armaan',
+      lastName: 'Singh',
+      email: 'armaan@example.com',
+      salary: '50000',
+      date: '2022-06-13'
+    },
+    {
+      id: 6,
+      firstName: 'Lissa',
+      lastName: 'Manna',
+      email: 'lissa@example.com',
+      salary: '60000',
+      date: '2022-06-30'
+    },
+    {
+      id: 7,
+      firstName: 'Priyanka',
+      lastName: 'Rawat',
+      email: 'priyanka@example.com',
+      salary: '70000',
+      date: '2022-04-15'
+    },
+    {
+      id: 8,
+      firstName: 'Jesica',
+      lastName: 'kumari',
+      email: 'jesica@example.com',
+      salary: '55000',
+      date: '2022-1-10'
+    },
+    {
+      id: 9,
+      firstName: 'Manoj',
+      lastName: 'Warner',
+      email: 'manoj@example.com',
+      salary: '80000',
+      date: '2022-3-18'
+    },
+    {
+      id: 10,
+      firstName: 'Tina',
+      lastName: 'Sen',
+      email: 'tina@example.com',
+      salary: '90000',
+      date: '2022-01-25'
+    }
+  ];
+  
+  export { employeesData };
